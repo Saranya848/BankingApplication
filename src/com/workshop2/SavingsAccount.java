7da0f0b4f552f0971cc5ac04614e8fe96659e9ce
SaranyaSiripurapu <85158690+SaranyaSiripurapu@users.noreply.github.com>
2022-03-08 16:37:17 +0530

    Update SavingsAccount.java

--- src/com/workshop2/SavingsAccount.java
+++ src/com/workshop2/SavingsAccount.java
@@ -1,28 +1,58 @@
 package com.workshop2;
 
 public class SavingsAccount {
-
+	/**
+	 * getSavingsBalance - it returns the savings balance amount
+	 * modifyInterestRate -  it modifies the interest with new interest
+	 * getAnnualInterestRate - it returns the interest rate
+	 * calculateMonthlyInterest -  calculate monthly interest
+	 * 
+	 */
 	private double savingBalance;
 	static private double annualInterestRate;
 
+	/**
+	 * Saving constructor
+	 * 
+	 * @param savingBalance
+	 */
 	public SavingsAccount(double savingBalance) {
 		this.savingBalance = savingBalance;
 	}
 
+	/**
+	 * returns the savings balance amount
+	 * 
+	 * @return
+	 */
 	// Get saving balance
 	public double getSavingBalance() {
 		return this.savingBalance;
 	}
 
+	/**
+	 * modifies the interest with new interest
+	 * 
+	 * @param newInterestRate
+	 */
 	public static void getInterestRate(double newInterestRate) {
 		annualInterestRate = newInterestRate;
 	}
 
+	/**
+	 * returns the interest rate
+	 * 
+	 * @param rate
+	 * @return
+	 */
 	public static double annualInterestRate(double rate) {
 		double annualInterestRate = rate / 100;
 		return annualInterestRate;
 	}
 
+	/**
+	 * calculate monthly interest
+	 */
 	public void calculateMonthlyInterest() {
 		System.out.println("Current savings balance: " + this.savingBalance);
 		double monthlyInterest;
