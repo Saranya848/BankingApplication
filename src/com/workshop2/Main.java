f0b82ad8f8489847000d97dd961e61a159cd01b8
SaranyaSiripurapu <85158690+SaranyaSiripurapu@users.noreply.github.com>
2022-03-08 16:36:06 +0530

    Update Main.java

--- src/com/workshop2/Main.java
+++ src/com/workshop2/Main.java
@@ -6,24 +6,46 @@ public class Main {
 		SavingsAccount saver1, saver2;
 		saver1 = new SavingsAccount(2000);
 		saver2 = new SavingsAccount(3000);
+		/**
+		 * Procedure
+		 * Call method monthlyInterset
+		 * Printing updated balance of two savers
+		 * 
+		 */
 
-		// Set the annual interest rate to 4%=0.04
+		/**
+		 * Set the annual interest rate to 4%=0.04
+		 */
 		SavingsAccount.getInterestRate(0.04);
 
-		// Calculate monthly interest
+		/**
+		 * calling Calculate monthly interest
+		 */
 		saver1.calculateMonthlyInterest();
 		saver2.calculateMonthlyInterest();
 
-		// Print out the new balances for both savers
+		/**
+		 * 
+		 * Print out the new balances for both savers
+		 */
 		System.out.println("This month:\nSaver 1 balance= " + saver1.getSavingBalance());
 		System.out.println("Saver 2 balance= " + saver2.getSavingBalance());
 
-		// Change annual interest rate to 5%=0.05
+		/**
+		 * Change annual interest rate to 5%=0.05
+		 */
 		SavingsAccount.getInterestRate(0.05);
 
-		// Calculate the next month interest rate and print out balances
+		/**
+		 * Calculate the next monthly interest rate
+		 */
 		saver1.calculateMonthlyInterest();
 		saver2.calculateMonthlyInterest();
+
+		/**
+		 * 
+		 * Print out the new balances for both savers
+		 */
 		System.out.println("Next month:\nSaver 1 balance= " + saver1.getSavingBalance());
 		System.out.println("Saver 2 balance= " + saver2.getSavingBalance());
 	}
